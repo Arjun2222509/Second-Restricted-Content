122c2a0754f610d2d55f8629f2f980d0288857b4
Arjun2222509 <drono0176@gmail.com>
2025-03-28 19:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ADMINS = int(os.environ.get("ADMINS", "7587316405"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://ronod723:ZStrBDOpn2oKzCcO@arjun4.tujdmxl.mongodb.net/?retryWrites=true&w=majority&appName=ARJUN4") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://habhi947725:rDDfFuZDMjcb8IPW@cluster0.bevc8vc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "ARJUN4")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
